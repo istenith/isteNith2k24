bab4254c13e26b10c107e733c7914696c8172ec3
ashutosh <ashutoshsadana10@gmail.com>
2024-07-02 00:08:33 +0530

    new commit

---
+++ src/app/gallery/page.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from 'next';
+
+const Blogs: NextPage = () => {
+    return (
+        <div>
+            <h1>blogs Page for Students</h1>
+        </div>
+    );
+};
+
+export default Blogs;
