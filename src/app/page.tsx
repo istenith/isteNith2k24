13529b705f584173252f740aba7845d4f575146a
ashutosh <ashutoshsadana10@gmail.com>
2024-07-02 16:02:43 +0530

    new

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
-
 "use client"
 import Link from 'next/link';
-
-import {useRouter} from 'next/navigation'
+import {useRouter} from 'next/navigation';
 import styles from './page.module.css';
 import Image from 'next/image';
 
@@ -11,36 +9,40 @@ import Head from 'next/head';
 
 
 const Homepage = () => {
-  const navItems = [
-      { name: 'HOME', href: '/' },
-    { name: 'MEMBERS', href: '/members'},
-    { name: 'EVENTS', href: '/events'},
-    { name: 'GALLERY', href: '/gallery'},
-     { name: 'BLOGS', href: '/blogs'},
-    { name: 'PROJECTS', href: '/projects'},
+  const pagenames = [
+    { name: 'HOME', href: '/' },
+    { name: 'MEMBERS', href: '/members' },
+    { name: 'EVENTS', href: '/events' },
+    { name: 'GALLERY', href: '/gallery' },
+    { name: 'BLOGS', href: '/blogs' },
+    { name: 'PROJECTS', href: '/projects' },
   ];
 
   return (
     <div className="h-screen bg-[#161616] text-white">
       <img src="iste_logo-01-removebg-preview.png" className="w-20 h-auto" />
-      <span className=" text-white text-opacity-60 font-thin ml-28 text-7xl">NITH</span>
+      <span className="text-white text-opacity-60 font-thin ml-28 text-7xl">NITH</span>
 
-     
-        <h1 className="text-8xl font-bold drop-shadow-[0_8px_5px_rgb(255,255,255)] 
-      text-shadow: 2px 2px 4px #00000 font-9xl ml-96 mt-60 ">ISTE</h1>
+
+
+      <h1 className="  text-20xl font-bold raleway-regular  ml-96 -mt-60">ISTE</h1>
+
+
+      <h1 className=" text-white text-opacity-50 font-bold raleway-regular  text-20xl  ml-96 -mt-80 " style={{ clipPath: 'inset(60% 0 0 0)' }}>ISTE</h1>
+      <h1 className=" text-white text-opacity-20 font-bold raleway-regular  text-20xl  ml-96 -mt-96 " style={{ clipPath: 'inset(60% 0 0 0)' }}>ISTE</h1>   
       
-
-      <nav className="absolute top-1/2 right-0  pr-20 text-white">
-        <ul className ="space-y-5">
-          {navItems.map((item) => (
-            <li key={item.name}   className="relative group  text-2xl flex items-center">
+      
+         <nav className="absolute top-44  right-0 pr-20 text-white">
+        <ul className="space-y-5">
+          {pagenames.map((item) => (
+            <li key={item.name} className="relative group text-2xl flex items-center">
               <Link href={item.href}>
                 <div className="text-white transform transition-transform duration-300 group-hover:-translate-y-1.5 mr-4 cursor-pointer">
                   {item.name}
                 </div>
               </Link>
-              <span className="block w-24 h-px bg-white"></span>
-              <span className="block w-3 h-3   bg-white rounded-full"   ></span>
+              <span className="flex w-24 h-px bg-white"></span>
+              <span className="flex w-3 h-3 bg-white rounded-full"></span>
             </li>
           ))}
         </ul>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,30 @@
 
 module.exports = {
+  
+  theme: {
+    extend: {},
+  },
+  fontSize: {
+    '10xl': '10rem', // Define a larger size as needed
+    '12xl': '12rem',
+    '14xl': '14rem',
+  },
+  plugins: [],
+};
+
+module.exports = {
   content: [
     "./src/**/*.{js,ts,jsx,tsx}",
    ,
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        '28xl': '28rem', // Define a larger size as needed
+        '20xl': '20rem',
+        '24xl': '24rem',
+      },
+    },
   },
   plugins: [],
-};
+}
