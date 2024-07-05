bcf808449e970013f1dd4b32d1f34de6d9e5a3ec
Amit-Singh_Bathyal <ammybathyal@gmail.com>
2024-07-05 16:23:43 +0530

    events page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,24 +7,15 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    fontFamily: {
-      'sans': ['Raleway', 'sans-serif'],
-    },
     
-    extend: {
-      colors: {
-        charcoal: '#161616',
+    extend: {},
+
+      fontFamily: {
+        desc: ['Encode Sans Expanded'],
+        head: ['Raleway'],
       },
 
-      letterSpacing: {
-        'expanded': '0em', 
-      },
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+    
   },
   plugins: [],
 };
