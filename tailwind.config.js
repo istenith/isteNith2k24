683d62040581e527d042d9b04b88981933a00e9a
ashutosh <ashutoshsadana10@gmail.com>
2024-07-12 16:57:15 +0530

    update home page added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,37 @@
 
 module.exports = {
-  
-  theme: {
-    extend: {},
-  },
-  fontSize: {
-    '10xl': '10rem', // Define a larger size as needed
-    '12xl': '12rem',
-    '14xl': '14rem',
-  },
-  plugins: [],
-};
-
-module.exports = {
   content: [
     "./src/**/*.{js,ts,jsx,tsx}",
    ,
   ],
   theme: {
     extend: {
-      fontSize: {
-        '28xl': '28rem', // Define a larger size as needed
-        '20xl': '20rem',
-        '24xl': '24rem',
+      translate: {
+        'z-10': '10px',
+        'z-20': '20px',
+        'z-30': '30px',
+        // Add more as needed
       },
     },
   },
-  
-  plugins: [],
-}
+  variants: {
+    extend: {
+      translate: ['hover', 'focus'],
+    },
+  },
+  plugins: [
+    function ({ addUtilities }) {
+      addUtilities({
+        '.hide-scrollbar': {
+          /* Hide scrollbar for Chrome, Safari, and Opera */
+          '&::-webkit-scrollbar': {
+            display: 'none',
+          },
+          /* Hide scrollbar for IE, Edge, and Firefox */
+          '-ms-overflow-style': 'none',  /* IE and Edge */
+          'scrollbar-width': 'none',  /* Firefox */
+        },
+      });
+    },
+  ],
+};
