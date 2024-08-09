76e71de097d5c14fa6d6f64dd0e550688eb2767d
akshit Pathania <akshitpathania75@gmail.com>
2024-08-09 15:33:15 +0530

    navbar fixed

--- src/components/navbar1.jsx
+++ src/components/navbar1.jsx
@@ -75,8 +75,8 @@ ISTE
         </div>
       </div>
       {isOpen && (
-        <div className="absolute top-16 left-0 w-full h-screen  bg-[#171500] z-40 md:hidden">
-          <div className="flex flex-col mx-[40%] space-y-4 pt-7">
+        <div className="absolute top-16 left-0 w-full h-screen  bg-[#171616] z-40 md:hidden">
+          <div className="flex flex-col mx-[40%] space-y-2 pt-2 pb-7">
             {navbarItems.map((item, index) => (
               <Link href={item.link} key={index} className="text-white text-2xl  no-underline hover:text-gray-300" onClick={toggleMenu}>
                 {item.title}
